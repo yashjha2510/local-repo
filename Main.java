d95958e8e4dcdbc6770960bb6a0ce84f3bf5854b
yashjha2510 <yashjha2510@gmail.com>
2025-07-19 20:26:38 +0530

    add a line by main

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("hello world!");   
-        System.out.println("this line is add in branch1");     
+        System.out.println("this line is add in branch1");    
+        System.out.println("line by main"); 
     }
 }
